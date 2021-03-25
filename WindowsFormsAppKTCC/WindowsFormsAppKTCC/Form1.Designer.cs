6527994dfbf507de2d45b8296d3df54f0fd69025
Phuoc <nguyendaiphuoc169@gmail.com>
2021-03-25 12:08:40 +0700

    sadsad

--- WindowsFormsAppKTCC/WindowsFormsAppKTCC/Form1.Designer.cs
+++ WindowsFormsAppKTCC/WindowsFormsAppKTCC/Form1.Designer.cs
@@ -29,33 +29,56 @@ namespace WindowsFormsAppKTCC
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.lbUserName = new System.Windows.Forms.Label();
             this.txtUserName = new System.Windows.Forms.TextBox();
+            this.LbPassword = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // label1
+            // lbUserName
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(61, 44);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(79, 17);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "User Name";
+            this.lbUserName.AutoSize = true;
+            this.lbUserName.Location = new System.Drawing.Point(46, 36);
+            this.lbUserName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbUserName.Name = "lbUserName";
+            this.lbUserName.Size = new System.Drawing.Size(60, 13);
+            this.lbUserName.TabIndex = 0;
+            this.lbUserName.Text = "User Name";
             // 
             // txtUserName
             // 
-            this.txtUserName.Location = new System.Drawing.Point(177, 44);
+            this.txtUserName.Location = new System.Drawing.Point(133, 36);
+            this.txtUserName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtUserName.Name = "txtUserName";
-            this.txtUserName.Size = new System.Drawing.Size(100, 22);
+            this.txtUserName.Size = new System.Drawing.Size(100, 20);
             this.txtUserName.TabIndex = 1;
+            // 
+            // LbPassword
+            // 
+            this.LbPassword.AutoSize = true;
+            this.LbPassword.Location = new System.Drawing.Point(46, 68);
+            this.LbPassword.Name = "LbPassword";
+            this.LbPassword.Size = new System.Drawing.Size(53, 13);
+            this.LbPassword.TabIndex = 2;
+            this.LbPassword.Text = "Password";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(133, 65);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 3;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.LbPassword);
             this.Controls.Add(this.txtUserName);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbUserName);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -65,8 +88,10 @@ namespace WindowsFormsAppKTCC
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbUserName;
         private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label LbPassword;
+        private System.Windows.Forms.TextBox txtPassword;
     }
 }
 
